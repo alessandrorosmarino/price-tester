fc2f3ebd4bd31d237b007f0ca8f91e19f7515223
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-10-31 09:32:46 +0000

    Project Initialization
    Initialized Project with starting dependencies

---
+++ src/main/java/com/bnc/price/tester/pricetester/PriceTesterApplication.java
@@ -0,0 +1,13 @@
+package com.bnc.price.tester.pricetester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnc/price/tester/pricetester/PriceTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnc.price.tester.pricetester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
